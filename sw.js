b242a1b514170ef80e1f37dc3c12a57758ad0360
vipera8 <vipera8@seznam.cz>
2026-06-27 17:28:02 +0200

    úprava obtížnosti hry
    obtížnost

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='grollova-cesta-v152-duration-copy';
-const STATIC=['./','./index.html','./404.html','./style.css','./style.css?v=152','./app.js','./app.js?v=152','./game-data.js','./game-data.js?v=152','./manifest.webmanifest','./assets/images/certifikat-bez-titulu.jpg','./assets/images/groll_uvod.jpg','./assets/images/Groll_logo_na_sirku.jpg','./assets/images/Groll_logo_na_vysku.jpg','./assets/images/06_velka_synagoga_reseni_trojuhelniky.png','./assets/images/web_darkovy_voucher_fade.png','./assets/images/web_klice.jpg','./assets/images/selfie_frame_landscape.png'];
+const CACHE='grollova-cesta-v153-difficulty-copy';
+const STATIC=['./','./index.html','./404.html','./style.css','./style.css?v=153','./app.js','./app.js?v=153','./game-data.js','./game-data.js?v=153','./manifest.webmanifest','./assets/images/certifikat-bez-titulu.jpg','./assets/images/groll_uvod.jpg','./assets/images/Groll_logo_na_sirku.jpg','./assets/images/Groll_logo_na_vysku.jpg','./assets/images/06_velka_synagoga_reseni_trojuhelniky.png','./assets/images/web_darkovy_voucher_fade.png','./assets/images/web_klice.jpg','./assets/images/selfie_frame_landscape.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
